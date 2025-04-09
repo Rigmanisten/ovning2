f0b3e29fd280e7268d2c133694a604b3ff139c1b
linus <linus00552@gmail.com>
2025-04-09 10:43:57 +0200

    Recoring metod

--- src/main/java/se/su/ovning2/Recording.java
+++ src/main/java/se/su/ovning2/Recording.java
@@ -1,6 +1,7 @@
 package se.su.ovning2;
 
 import java.util.Collection;
+import java.util.Objects;
 import java.util.Set;
 
 public class Recording {
@@ -9,6 +10,7 @@ public class Recording {
   private final String title;
   private final String type;
   private final Set<String> genre;
+
 
   public Recording(String title, String artist, int year, String type, Set<String> genre) {
     this.title = title;
@@ -41,5 +43,18 @@ public class Recording {
   @Override
   public String toString() {
     return String.format("{ %s | %s | %s | %d | %s }", artist, title, genre, year, type);
+  }
+ @Override
+  public boolean equals(Object obj){
+    if(this == obj) return true;
+    if(!(obj instanceof Recording)) return false;
+    Recording rec = (Recording) obj;
+    return year == rec.year &&
+    artist.equals(rec.artist) &&
+    title.equals(rec.title);
+  }
+  @Override
+  public int hashCode() {
+    return Objects.hash(title, artist, year);
   }
 }
